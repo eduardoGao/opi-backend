b921cc4267dcaf8e8b94a905c6f2d62f451fa6a3
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-03-28 19:32:53 -0600

    :bug: [UPDATE] date questions post

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-  PORT: process.env.port || 3000,
+  PORT: process.env.PORT || 3000,
   DB_USER: process.env.DB_USER,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   DB_NAME: process.env.DB_NAME
